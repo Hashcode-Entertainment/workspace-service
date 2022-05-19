0c382e14221ad72d954a69fc63f745a2b84533b6
Paweł Kiełb <pawel.kielb@protonmail.com>
2022-05-19 18:22:22 +0200

    Switch to Kotlin Gradle DSL

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    java
+}
+
+group = "com.workspaceservice"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly.get().extendsFrom(configurations.annotationProcessor.get())
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
